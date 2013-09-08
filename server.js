4c9f729327bb6285eecd537498d4199957d98431
Jaeman An <ajmbell@bawi.org>
2013-09-08 12:06:44 +0900

    re-construct

--- server.js
+++ server.js
@@ -4,8 +4,8 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
+var routes = require('./config/routes');
+var user = require('./config/routes/user');
 var http = require('http');
 var path = require('path');
 
